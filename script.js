a7548ab817d9024ed6b2c42c9a95bc599175b22f
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2024-03-17 13:27:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 //complete this code
 class Animal {
-	constructor(species: string){
+	constructor(species){
 		this._species;
 		
 		
 	}
-	get species(){
-		return this._species;
+	get makeSound(){
+		return this.makeSound;
 	}
-	set species(){
-		this._species=species;
+	set makeSound(){
+		this.makeSound=makeSound;
 	}
 }
 
 class Dog extends Animal {
 	bark(){
-		console.log(`${this.species}`)
+		console.log(`${this.makeSound}`)
 	}
 }
 
 class Cat extends Animal {
 	purr(){
-		console.log(`${this.species}`)
+		console.log(`${this.makeSound}`)
 	}
 }
 
