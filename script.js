d8ce129ef816fbca8102719f18a74780dd27b9c4
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2024-03-22 16:31:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -38,6 +38,13 @@
 // Today
 
 // Team Acciojob  to  You (direct message) 13:41
+// Today
+
+// You  to  Team Acciojob (direct message) 16:15
+// hello sir
+// hello sir
+
+Team Acciojob  to  You (direct message) 16:27
 class Animal {
   constructor(species) {
     this._species = species;
@@ -45,12 +52,8 @@ class Animal {
   get species() {
     return this._species;
   }
-	set species(){
-		this._species=species;
-	}
-
   makeSound() {
-    console.log(`"The " + ${this.species} + " makes a sound"`);
+    console.log("The " + this.species + " makes a sound");
   }
 }
 class Cat extends Animal {
@@ -67,4 +70,5 @@ class Dog extends Animal {
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
+// Messages addressed to "Meeting Group Chat" will also appear in the meeting group chat in Team Chat
 // Messages addressed to "Meeting Group Chat" will also appear in the meeting group chat in Team Chat
